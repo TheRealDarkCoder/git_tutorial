5f812a44ca906c99671d00c258115aff355bd3c0
TheRealDarkCoder <zuhayr2003@gmail.com>
2022-09-26 19:52:11 +0600

    Changed value to 50

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ def modular_difference(x, y):
     if(x > y):
         return x - y
     return y - x
-print(modular_difference(100, 100))
+print(modular_difference(100, 50))
 ## Backend stuff
 ## Frontend stuff
